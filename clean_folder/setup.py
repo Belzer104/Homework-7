157c8808971610d6911c7c1f92f492fe8ae9b09e
Belzer104 <shablovskyiillia@gmail.com>
2022-10-30 00:16:27 +0300

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,5 +1,4 @@
-from importlib.metadata import entry_points
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name= "clean_folder",
@@ -7,8 +6,8 @@ setup(
     author= "Illia",
     url= "hhttps://github.com/Belzer104",
     license= "MIT",
-    packages= find_packages(),
-    description = "Sorted file"
+    packages= find_namespace_packages(),
+    description = "Sorted file",
     long_description= open("README.md").read(),
     entry_points= {'console_scripts': ['clean-folder = clean_folder.clean:run_sort']}   
 )
