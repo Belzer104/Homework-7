e01929d3f432ef5a38130d81fc1bed41b39bbbc8
Belzer104 <shablovskyiillia@gmail.com>
2022-10-29 23:35:56 +0300

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,3 +1,4 @@
+from importlib.metadata import entry_points
 from setuptools import setup, find_packages
 
 setup(
@@ -8,6 +9,6 @@ setup(
     license=open("LICENCE").read(),
     packages= find_packages(),
     long_description= open("README.md").read(),
-
+    entry_points= {'console_scripts': ['clean-folder = clean_folder.clean:clean_folder']}
     
 )
