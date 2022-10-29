26688bd7d97f4a1ecd6291740d04ecd48c982d29
Belzer104 <shablovskyiillia@gmail.com>
2022-10-29 23:31:05 +0300

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
+    name= "clean_folder",
+    version= "0.0.1",
+    author= "Illia",
+    url= "hhttps://github.com/Belzer104",
+    license=open("LICENCE").read(),
+    packages= find_packages(),
+    long_description= open("README.md").read(),
+
     
 )
